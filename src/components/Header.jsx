07ea5322edde938f8c72ee3eb7d0af9904f7bd33
vikissj5 <vikissj5@gmail.com>
2024-02-28 20:50:08 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-
+import './Header.css'
+import { Height } from '@mui/icons-material';
 const Header = () => {
   const imageUrl = 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-original-577x577/s3/082016/untitled-1_203.jpg?itok=ZKsBl7YG';
 
   return (
     <header>
-      <div>
+      <div className='header'>
         {/* Use an absolute path to reference the image in the public folder */
-        <img src={imageUrl} alt="Header" />
-  }
+          <img style={{
+            width: "10%",
+            height: "100%"
+          }} src={imageUrl} alt="Header" />
+        }
       </div>
     </header>
   );
